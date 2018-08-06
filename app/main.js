7347acdccc6b8faa59c33aa741ab4e0f6d8cdb09
Michael Green <michael.green94@hotmail.com>
2018-08-06 15:14:20 -0600

    imported files to main.js

--- app/main.js
+++ app/main.js
@@ -1,3 +1,32 @@
+import Afanc from './Models/Afanc.js'
+import Bear from './Models/Bear.js'
+import Centaur from './Models/Centaur'
+import Dragon from './Models/Dragon'
+import Elf from './Models/Elf'
+import Fair from './Models/Fairy'
+import Goblin from './Models/Goblin'
+import Hydra from './Models/Hydra'
+import Itcuintlipotzotli from './Models/Itcuintlipotzotli'
+import Jackle from './Models/Jackle'
+import Kraken from './Models/Kraken'
+import Lion from './Models/Lion'
+import Minotaur from './Models/Minotaur'
+import Nymph from './Models/Nymph'
+import Orc from './Models/Orc'
+import Pegasus from './Models/Pegasus'
+import Quanlier from './Models/Quanlier'
+import Roc from './Models/Roc'
+import Succubus from './Models/Succubus'
+import Troll from './Models/Troll'
+import Unicorn from './Models/Unicorn'
+import Vampire from './Models/Vampire'
+import Warthog from './Models/Warthog'
+import XingTian from './Models/XingTian'
+import Yeti from './Models/Yeti'
+import Zombie from './Models/Zombie'
+
+
+
 
 
 // let simba = new Lion('Simba')
