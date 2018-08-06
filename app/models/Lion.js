287017f3041faeebe9051d7e569a185b4fee8503
Michael Green <michael.green94@hotmail.com>
2018-08-06 10:34:45 -0600

    following along

---
+++ app/models/Lion.js
@@ -0,0 +1,12 @@
+class Lion{
+
+    //What are the form fields?
+    constructor(name, hairColor, gender, maneColor, weight) {  
+        this.name = name;
+        this.hairColor = hairColor;
+        this.gender = gender;
+        this.maneColor = maneColor;
+        this.weight = weight;
+    }
+
+}
